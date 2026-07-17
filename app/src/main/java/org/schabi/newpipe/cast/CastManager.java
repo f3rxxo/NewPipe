5445c18eb5d8415b3514aeed3a578dd993921180
f3rxxo <112unchart3d@gmail.com>
2026-07-17 11:55:01 -0500

    hasConnectedSession method addes

--- app/src/main/java/org/schabi/newpipe/cast/CastManager.java
+++ app/src/main/java/org/schabi/newpipe/cast/CastManager.java
@@ -2,7 +2,9 @@ package org.schabi.newpipe.cast;
 
 import android.app.Activity;
 import android.util.Log;
+import android.net.Uri;
 
+import androidx.annotation.NonNull;
 import androidx.mediarouter.app.MediaRouteChooserDialog;
 import androidx.mediarouter.media.MediaRouteSelector;
 
@@ -10,6 +12,15 @@ import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.cast.framework.CastContext;
 import com.google.android.gms.cast.framework.CastSession;
 import com.google.android.gms.cast.framework.SessionManager;
+import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.MediaLoadRequestData;
+import com.google.android.gms.cast.MediaMetadata;
+import com.google.android.gms.cast.MediaMetadata.MEDIA_TYPE_MOVIE;
+import com.google.android.gms.cast.framework.media.RemoteMediaClient;
+import com.google.android.gms.common.images.WebImage;
+
+import org.schabi.newpipe.extractor.stream.VideoStream;
+import org.schabi.newpipe.player.mediaitem.MediaItemTag;
 
 public final class CastManager {
 
@@ -33,6 +44,26 @@ public final class CastManager {
     }
     public static boolean hasConnectedSession(final Activity activity) {
 
+        public static void cast(final Activity activity,
+                                @NonNull final MediaItemTag tag,
+                                @NonNull final VideoStream stream) {
+
+        final CastSession session =
+                CastContext.getSharedInstance(activity)
+                        .getSessionManager()
+                        .getCurrentCastSession();
+
+        if (session == null || !session.isConnected()) {
+            Log.w(TAG, "No active Cast session");
+            return;
+        }
+
+        final RemoteMediaClient remoteMediaClient = session.getRemoteMediaClient();
+
+        if (remoteMediaClient == null) {
+            Log.w(TAG, "RemoteMediaClient unavailable");
+            return;
+        }
         final CastContext castContext = CastContext.getSharedInstance(activity);
 
         final SessionManager sessionManager = castContext.getSessionManager();
