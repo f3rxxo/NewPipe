d8b378476133ee03d7611f089f2904140cf35173
f3rxxo <112unchart3d@gmail.com>
2026-07-17 17:36:50 -0500

    CastManager fix

--- app/src/main/java/org/schabi/newpipe/cast/CastManager.java
+++ app/src/main/java/org/schabi/newpipe/cast/CastManager.java
@@ -15,7 +15,6 @@ import com.google.android.gms.cast.framework.SessionManager;
 import com.google.android.gms.cast.MediaInfo;
 import com.google.android.gms.cast.MediaLoadRequestData;
 import com.google.android.gms.cast.MediaMetadata;
-import com.google.android.gms.cast.MediaMetadata.MEDIA_TYPE_MOVIE;
 import com.google.android.gms.cast.framework.media.RemoteMediaClient;
 import com.google.android.gms.common.images.WebImage;
 
@@ -78,7 +77,7 @@ public final class CastManager {
         }
 
         final MediaMetadata metadata =
-                new MediaMetadata(MEDIA_TYPE_MOVIE);
+        new MediaMetadata();
 
         metadata.putString(MediaMetadata.KEY_TITLE, tag.getTitle());
         metadata.putString(MediaMetadata.KEY_SUBTITLE, tag.getUploaderName());
